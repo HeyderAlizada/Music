b3e2ce9a2bc96e01685905bafbd385ef73035a2a
HeyderAlizada <hydrealyev@gmail.com>
2025-02-15 22:08:14 +0400

    To-do

--- MyReactApp/src/App.jsx
+++ MyReactApp/src/App.jsx
@@ -2,13 +2,13 @@ import { useState, useEffect } from 'react';
 import './App.css'
 
 function App() {
-  const [color, setcolor] = useState("blue");
 
   return (
-    <>
-      <h1 color='white'>I like <span style={{color : color}}>{color}</span> </h1>
-      <button onClick={() => { setcolor(color === "blue" ? "red" : "blue") }}>change to {color === "blue" ? "red" : "blue"}</button>
-    </>
-  )
+    
+      <div className='to-do'>
+        <input type="text" placeholder='Enter a task'/>
+        <button>ADD</button>
+      </div>
+    )
 }
 export default App
