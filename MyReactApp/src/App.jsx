ea6b2ed95be73252fc6c3b445a1c3f548949ae9f
HeyderAlizada <hydrealyev@gmail.com>
2025-02-23 01:20:25 +0400

    small changes

--- MyReactApp/src/App.jsx
+++ MyReactApp/src/App.jsx
@@ -1,14 +1,22 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, createElement,useRef } from 'react';
 import './App.css'
 
 function App() {
 
+  const [text, settext] = useState();
+
+  const inside= ()=>{
+    settext();
+  }
+
   return (
-    
+    <>
       <div className='to-do'>
-        <input type="text" placeholder='Enter a task'/>
-        <button>ADD</button>
+        <input type="text" placeholder='Enter a task' onChange={inside()} />
+        <button onClick={addTask}>ADD</button>
       </div>
-    )
+      {task}
+    </>
+  )
 }
 export default App
