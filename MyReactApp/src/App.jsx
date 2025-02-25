ce36b808da2bb84f5209843913f5e8c0d624a8d8
HeyderAlizada <hydrealyev@gmail.com>
2025-02-25 14:24:37 +0400

    some quality life changes

--- MyReactApp/src/App.jsx
+++ MyReactApp/src/App.jsx
@@ -1,21 +1,34 @@
-import { useState, useEffect, createElement,useRef } from 'react';
+import { useState, useEffect, createElement, useRef } from 'react';
 import './App.css'
 
 function App() {
 
-  const [text, settext] = useState();
+  const [text, settext] = useState("");
+  const [tasks, settasks] = useState([]);
+  const handleChange = (event) => {
+    settext(event.target.value);
+  }
 
-  const inside= ()=>{
-    settext();
+  const addTask = () => {
+    if (text.trim() !== "") {
+      settasks([...tasks, text]);
+      settext("");
+    }
   }
 
   return (
     <>
       <div className='to-do'>
-        <input type="text" placeholder='Enter a task' onChange={inside()} />
+        <input type="text" placeholder='Enter a task' value={text} onChange={handleChange} />
         <button onClick={addTask}>ADD</button>
       </div>
-      {task}
+      <div className="listof">
+        <ul>
+          {tasks.map((task, index) => (
+            <div className="lists"><li key={index}>{index+1}) {task}</li><button>Delete</button></div>
+          ))}
+        </ul>
+      </div>
     </>
   )
 }
